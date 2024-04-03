327c66c474e93a7d40924c964f53a24dac283020
harunsefainan <harunsefa.inan@gmail.com>
2024-04-04 00:03:30 +0300

    user repo update

--- src/main/java/com/harunsefainan/ecommerce/repository/UserRepository.java
+++ src/main/java/com/harunsefainan/ecommerce/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.harunsefainan.ecommerce.repository;
 
-import org.apache.catalina.User;
+import com.harunsefainan.ecommerce.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    UserEntity findByUsername(String username);
 }
